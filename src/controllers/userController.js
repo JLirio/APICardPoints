e2866fd059530a29afb5890d7bde3371920b88fd
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-29 00:49:23 -0300

    userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,12 +13,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const prisma = new PrismaClient()
 const secretKey = "sua-chave-secreta"; // Deve ser um segredo seguro.
 
-// Configuração da API do Google Drive
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
 // Configuração do Multer para upload local antes de enviar para o Google Drive
 export const upload = multer({
     storage: multer.diskStorage({
