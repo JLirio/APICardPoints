9f7746a638109adf6ea4912ee227a18f70a49844
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-29 00:47:47 -0300

    dotenv config

--- src/app.js
+++ src/app.js
@@ -2,14 +2,18 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routers/userRoutes.js";
 import adminRoutes from "./routers/adminRoutes.js";
+import dotenv from "dotenv"
 
 const app = express();
 
+dotenv.config()
 app.use(express.json());
 app.use(cors());
 
 // Rotas
 app.use("/usuarios", userRoutes);
 app.use("/admin", adminRoutes);
+
+//console.log(process.env.DATABASE_URL)
 
 export default app;
