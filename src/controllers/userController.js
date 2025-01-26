310ef07435ef4b1947179d84ffec5c072fea86ef
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-01-26 18:46:47 -0300

    GoogleCloud credentials in .env

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,10 +29,20 @@ export const upload = multer({
     },
 });
 
-
 const keyPath = path.join(__dirname, 'credentials.json');  // Caminho do arquivo JSON
 const auth = new google.auth.GoogleAuth({
-    keyFile: keyPath,
+    credentials: {
+        type: "service_account",
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    },
     scopes: ['https://www.googleapis.com/auth/drive.file'],  // Permissões necessárias
 });
 
@@ -42,7 +52,7 @@ const drive = google.drive({ version: 'v3', auth });
 
 // Função para fazer upload no Google Drive
 export const uploadToGoogleDrive = async (filePath, fileName, folderId) => {
-    
+
     const fileMetadata = {
         name: fileName,
         parents: [folderId],  // ID da pasta do Google Drive
